26015fa78169d32f8f7ee89f7f290b8d45127189
brickhill <petergibson@sbsys.co.uk>
2023-11-27 22:03:49 +0000

    Get secret key from .env

--- sbssite/settings/production.py
+++ sbssite/settings/production.py
@@ -2,7 +2,8 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ["http://whitchurch.pythonanywhere.com"]
-
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY") if "DJANGO_SECRET_KEY" in os.environ else ""
 try:
     from .local import *
 except ImportError:
