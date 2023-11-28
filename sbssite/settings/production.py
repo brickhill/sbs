18637bf26e0de1443a244414945b2330da5ad41f
brickhill <petergibson@sbsys.co.uk>
2023-11-28 21:56:08 +0000

    add whitchurch.pythonanywhere.com

--- sbssite/settings/production.py
+++ sbssite/settings/production.py
@@ -2,7 +2,8 @@ from .base import *
 
 
 DEBUG = False
-ALLOWED_HOSTS = ["http://whitchurch.pythonanywhere.com"]
+ALLOWED_HOSTS = ["http://whitchurch.pythonanywhere.com",
+"whitchurch.pythonanywhere.com"]
 
 try:
     from .local import *
