f8063d004cd91b20e5c6c82a94ae835d105d3c0d
brickhill <petergibson@sbsys.co.uk>
2025-04-22 20:45:49 +0100

    Login and logout

--- pages/urls.py
+++ pages/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
     path('', views.home, name="home" )
 ]
 
--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,61 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.db.models import Q
+# from django.http import HttpResponse
+# from django.contrib.auth.decorators import login_required
+# from django.db.models import Q
 from django.contrib.auth import authenticate, login, logout
-# from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 def home(request):
     context = {}
     return render(request, 'home.html', context)
+
+
+def login_page(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        messages.info(request, "Already logged in dickhead")
+        return redirect('home')
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except Exception as e:
+            messages.error(request, f"User doesn't exist ({e})")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, 'Logged in')
+            return redirect('home')
+        else:
+            messages.error(request, "Username or password wrong")
+
+    context = {'page': page}
+    return render(request, "login_registration.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, 'logged out')
+    return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            messages.info(request, "User Created")
+            return redirect('home')
+        else:
+            messages.error(request, "An error occurred during registration")
+
+    return render(request, 'login_registration.html', {'form': form})
