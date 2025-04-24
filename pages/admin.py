f491241aa44c6f1b1711fbefa14cd54c943ae020
brickhill <petergibson@sbsys.co.uk>
2025-04-24 21:54:27 +0100

    Add blog posts

--- pages/admin.py
+++ pages/admin.py
@@ -1,13 +1,47 @@
 from django.contrib import admin
+from .models import Category, Tag, BlogPost
+from django import forms
 
-from .models import Category
+
+class CustomCategoryAdminForm(forms.ModelForm):
+    parent = forms.IntegerField()
+
+    class Meta:
+        model = Category
+        fields = '__all__'
+
 
 class CategoryAdmin(admin.ModelAdmin):
     date_hierarchy = 'updated'
     actions = []
     list_display = ['title', 'lft', 'rgt', 'level', 'created', 'updated']
     readonly_fields = ['created', 'updated']
-    fields = ['title', 'description', 'lft', 'rgt', 'level', 'created', 'updated']
+    fields = ['title', 'description', 'lft', 'rgt', 'parent',
+              'level', 'created', 'updated']
+    exclude = []
+    form = CustomCategoryAdminForm
+
+    def save_model(self, request, obj, form, change):
+        # Handle the extra field here if needed
+        parent = form.cleaned_data.get('parent')
+        # TODO How do I display a list of categories to pick a parent?
+        print(f"PARENT{parent}")
+        obj.lft = parent
+        # Perform any actions with extra_field_value
+        super().save_model(request, obj, form, change)
+
+
+class BLogPostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'updated'
+    actions = []
+    list_display = ['title', 'author', 'updated']
+    readonly_fields = ['created', 'updated']
+    fields = ['title', 'body', 'synopsis', 'author', 'status',
+              'start_time', 'end_time', 'categories', 'tags', 'created',
+              'updated']
     exclude = []
 
+
+admin.site.register(Tag)
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(BlogPost, BLogPostAdmin)
--- pages/models.py
+++ pages/models.py
@@ -4,14 +4,17 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     title = models.CharField(max_length=200, blank=False, null=False,
-    help_text="Category Name", validators=[])
+                             help_text="Category Name", validators=[])
     # WRITE Describe options for Charfield
     # WRITE Describe validation options for Charfield.
-    description = models.TextField(max_length=50, blank=True, null=True, 
-    help_text="Description",validators=[])
-    lft = models.IntegerField(blank=False, null=False, default=0, validators=[])
-    rgt = models.IntegerField(blank=False, null=False, default=0,validators=[])
-    level = models.IntegerField(default=1, blank=False, null=False, validators=[])
+    description = models.TextField(max_length=50, blank=True, null=True,
+                                   help_text="Description", validators=[])
+    lft = models.IntegerField(blank=False, null=False, default=0,
+                              validators=[])
+    rgt = models.IntegerField(blank=False, null=False, default=0,
+                              validators=[])
+    level = models.IntegerField(default=1, blank=False, null=False,
+                                validators=[])
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -27,63 +30,52 @@ class Category(models.Model):
             # models.Index(fields=['last', 'first'])
         ]
         unique_together = []
-# Title
-# Body
-# Synopsis
-# Created
-# Updated
-# Author (FK)
-# Status (Draft/Published)
-# Start Time
-# End Time
-# Categories (M/M)
-# Comments (FK)
-# Tags (M/M)
- 
-# Category
-# Title
-# Description
-# LFT
-# RGT
-# Level
- 
-# Comments
-# Who
-# Text
-# Post
-# LFT
-# RGT
-# Level
-
-# class Room(models.Model):
-#     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-#     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-#     name = models.CharField(max_length=200)
-#     description = models.TextField(null=True, blank=True)
-#     participants = models.ManyToManyField(
-#         User,
-#         related_name="participants",
-#         blank=True
-#         )
-#     updated = models.DateTimeField(auto_now=True)
-#     created = models.DateTimeField(auto_now_add=True)
-
-#     class Meta():
-#         ordering = ['-updated', '-created']
-
-#     def __str__(self):
-#         return self.name
 
 
-# class Message(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-#     body = models.TextField(null=True, blank=True)
-#     updated = models.DateTimeField(auto_now=True)
-#     created = models.DateTimeField(auto_now_add=True)
+class Tag(models.Model):
+    title = models.CharField(max_length=200, blank=False, null=False,
+                             help_text="Tag Name", validators=[])
 
-#     class Meta():
-#         ordering = ['-updated', '-created']
+    def __str__(self):
+        return self.title
 
-#     def __str__(self):
-#         return self.body[0:50]
+
+class BlogPost(models.Model):
+    DRAFT = "DRAFT"
+    PUBLISHED = "PUBISHED"
+    STATUS_CHOICES = ((DRAFT, "Draft"), (PUBLISHED, "Published"))
+
+    title = models.CharField(max_length=200, blank=False, null=False,
+                             help_text="Post title", validators=[])
+    body = models.TextField(max_length=20000, blank=False, null=False,
+                            help_text="Blog body")
+    synopsis = models.TextField(max_length=20000, blank=True, null=True,
+                                help_text="Synopsis", validators=[])
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES,
+                              blank=True, null=True, help_text='Status')
+    start_time = models.DateTimeField(help_text="Optional Start time",
+                                      db_index=True, blank=True,  null=True)
+    end_time = models.DateTimeField(help_text="Optional end time",
+                                    db_index=True, blank=True, null=True)
+    categories = models.ManyToManyField(Category, help_text="Categories")
+    tags = models.ManyToManyField(Tag, help_text="Tags", blank=True, null=True)
+    # TODO Why are tags (and categories) mandatory?
+    # TODO Add comments (hierarchical)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        # db_table_comment = "Blog post categories"
+        verbose_name = "Blog Post"
+        verbose_name_plural = "Blog Posts"
+        ordering = ["title", "-updated"]
+        indexes = [
+            # models.Index(fields=['last', 'first'])
+        ]
+        unique_together = []
+
+# TODO Comments: Who, Text, Post, LFT, RGT, Level
